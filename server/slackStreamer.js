c3f7c124640be42141d99d3ad0ead501beb084e7
Alejandro León <alejandro.leon@the-cocktail.com>
2014-10-14 16:03:53 +0200

    refactored streamer to pickup only the oldest messages by channel

--- server/slackStreamer.js
+++ server/slackStreamer.js
@@ -6,44 +6,63 @@ module.exports = function(app){
 
   };
 
-  SlackStreamer.prototype.getLastChangedAt = function(cb){
-    app.redisClient.get('oldestChangedAt', function(err, oldest){
+  var getLastChangedAt = function( groupId, cb){
+    app.redisClient.get('group:' + groupId + ':oldestChangedAt', function(err, oldest){
       cb(oldest);
     })
   };
 
-  var getChannelMessages = function(params, cb){
-    console.log('getting channel messages');
-    app.slackClient.groupsHistory(params, function(err, response, body){
-      console.log('got channel messages');
-      _.each(body['messages'], function(message, _index, _list){
-        if (!_.has(message, 'subtype')){
-          app.slackUsers.userInfo(message.user, function(err, userInfo){
-            if (userInfo !== undefined){
-              message.username = userInfo.real_name;
-              _.extend(message, userInfo);
-              console.log(message);
-              cb(null, {channel: params.channel, message: message});
-            }
-          });
+
+  var getMessageInfo = function(channel, cb, message){
+    if (!_.has(message, 'subtype')){
+      app.slackUsers.userInfo(message.user, function(err, userInfo){
+        if (userInfo !== undefined && userInfo !== null){
+          message.username = userInfo.real_name;
+          _.extend(message, userInfo);
+          console.log(message);
+          cb(null, {channel: channel, message: message});
         }
       });
+    }
+  };
+
+  var getChannelMessages = function(params, cb){
+    console.log('getting channel messages');
+    console.log(params);
+    getLastChangedAt( params.channelId, function(oldest){
+      if (oldest !== null) params.oldest = oldest;
+
+      app.slackClient.groupsHistory(params, function(err, response, body){
+        console.log('got channel messages');
+        // console.log(err);
+        // console.log(response);
+        // console.log(body);
+
+        if (body['messages'].length > 0 ){
+          var bindedGetMessageInfo = _.partial(getMessageInfo, params.channel, cb);
+          _.each(body['messages'], bindedGetMessageInfo);
+
+          var oldestMessage = _.max(body['messages'], function(message){ return parseFloat(message.ts) });
+          app.redisClient.set('group:' + params.channeldId + ':oldestChangedAt', oldestMessage.ts);
+        }
+
+      });
+
     });
+
+
   };
 
   SlackStreamer.prototype.getLastMessages = function(channelMatches, cb){
-    this.getLastChangedAt( function(oldest){
-      var now = Date.now() / 1000;
-      app.redisClient.set('oldestChangedAt', now);
+
       app.slackClient.groupList( function(err, response, body){
         console.log('in get groupList');
         var monitoringGroups = _.filter(body["groups"], function(group){ return channelMatches.test(group["name"]) });
         console.log('fetched monitoringGroups');
-        _.each(monitoringGroups, function(group) {  getChannelMessages({channel: group['id'], oldest: oldest }, cb) } );
+        _.each(monitoringGroups, function(group) {  getChannelMessages({channel: group['id']}, cb) } );
 
       }.bind(this) );
 
-    }.bind(this));
   };
 
   return new SlackStreamer(app);
