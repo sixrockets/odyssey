709e0d654f55dc6af6c4784c81a76cedd73f78bc
Manuel Albarrán <weap88@gmail.com>
2015-10-19 01:06:06 +0200

    Added command to messages

--- src/server/boot/adapters.js
+++ src/server/boot/adapters.js
@@ -8,6 +8,17 @@ module.exports = (app) => {
     if (result) cb(result)
   }
 
+  // check if the commandText is at beginning of message
+  function command(commandText, regExp, cb) {
+    const hasRegExp = (cb === void 0)
+    const callback = hasRegExp ? regExp : cb
+    const regexp = hasRegExp ? /.*/ : regExp
+    const commandRegexp = new RegExp(`^[/@#]?${commandText}\\s*(.*)$`, "i")
+    let result = commandRegexp.exec(this.text)
+    result = result && regexp.exec(result[1])
+    if (result) callback(result)
+  }
+
   function callAll(prop) {
     const args = Array.prototype.slice.call(arguments, 1)
     app.modules.Qx.map(app.bots, bot => {
@@ -15,6 +26,7 @@ module.exports = (app) => {
         if (bot[prop]) bot[prop].apply(bot, args)
       } catch (err) {
         console.log(err)
+        console.log(err.stack)
       }
     })
   }
@@ -22,11 +34,14 @@ module.exports = (app) => {
   function onEvent(event, responder) {
     event.send = responder
     event.hear = hear.bind(event)
+    event.command = command.bind(event)
     event.extend = extend
     event.sample = sample
     event.map = map
     event.http = rp
-
+    event.sendLocation = event.sendLocation || (location => {
+      event.send(`https://www.google.com/maps/search/${location.lat},${location.lon}`)
+    })
     callAll("onEvent", event)
 
     if (event.type === "message") {
