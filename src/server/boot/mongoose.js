5358bbbbca031b69927a4baea4fcdf3d5775d4da
cicloon <aleon.prof@gmail.com>
2015-10-18 13:13:56 +0200

    connect to mongo on boot

--- src/server/boot/mongoose.js
+++ src/server/boot/mongoose.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
   console.log('mongoooooose');
   app.models = require('../models/index')(app);
-  app.slackUsers = require('../slackUsers')(app);
+  app.slackUsers = require('../adapters/slackUsers')(app);
+  app.modules.mongoose.connect(app.config.mongodb.url);
 }
