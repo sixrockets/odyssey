ff59d7b2ce9c2c2684edb83176d937f36afac6d0
Manuel Albarrán <weap88@gmail.com>
2015-10-20 00:39:10 +0200

    Refactored FlickrBot and GiphyBot for use msg.hear

--- src/server/bots/giphyBot.js
+++ src/server/bots/giphyBot.js
@@ -1,13 +1,8 @@
-import { extend, sample, map } from "lodash"
-import { get } from "request"
-
-module.exports = (_app) => {
-
+export default (_app) =>
   class GiphyBot {
-    constructor(responder) {
+    constructor() {
       this.name = "GiphyBot"
       this.apiCallUrl = "http://api.giphy.com/v1/gifs/search"
-      this.responder = responder
       this.qs = {
         api_key: process.env.GIPHY_KEY,
         limit: 50,
@@ -15,32 +10,18 @@ module.exports = (_app) => {
       }
     }
 
-    testMessage(message) {
-      return /^[\w\-]+\.gif$/.test(message)
-    }
-
-    getImageName(message) {
-      return message.substr(0, message.length - 4).replace(/[-_+\s]+/g, " ").trim()
-    }
-
-    perfomRequest(query, cb) {
-      if (!query) return
-
-      const qs = extend({q: query}, this.qs)
-      const params = {json: true, url: this.apiCallUrl, qs: qs}
-      get(params, (_e, _r, body) => body && cb(body))
-    }
-
-    onMessage(message) {
-      const query = this.testMessage(message.text) && this.getImageName(message.text)
-
-      this.perfomRequest(query, body => {
-        const options = map(body.data, photo => photo.url)
-        if (!options[0]) return
-        message.send(`${query}: ${sample(options)}`)
+    onMessage(msg) {
+      msg.hear(/^([\w\-]+)\.gif$/, (match) => {
+        const query = match[1].replace(/[-_+\s]+/g, " ").trim()
+        if (!query) return
+        const qs = msg.extend({q: query}, this.qs)
+        const params = {json: true, url: this.apiCallUrl, qs: qs}
+        msg.http.get(params, (_e, _r, body) => {
+          if (!body) return
+          const options = msg.map(body.data, photo => photo.url)
+          if (!options[0]) return
+          msg.send(`${query}: ${msg.sample(options)}`)
+        })
       })
     }
   }
-
-  return GiphyBot
-}
